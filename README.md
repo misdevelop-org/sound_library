ab9b2abe06050caad502c115c9b22311a45a0da2
Edgerik Leguizamon <edgerik.leguizamon@gmail.com>
2022-01-31 20:52:10 -0600

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# sound_library
- Easy UI sounds for your app
+
+## Features
+
+Easy Free UI sounds library for your app
+
+## Getting started
+
+Add this import line
+
+```dart
+import 'package:sound_library/sound_library.dart';
+```
+
+
+## Usage
+
+Upload an image to given path
+
+```dart
+  String url = await FireUploader(path:'/collectionName/Image.png').postImage(file);
+```
+
+Or upload directly from bytes (Uint8List) to given path
+
+```dart
+  String url = await FireUploader(path:'/collectionName/Image.png').postImageFromUint8List(bytes);
+```
+
+Select images from gallery and upload them
+
+```dart
+  String url = await FireUploader(path:'/collectionName/Image.png',maxImagesCount: 5).selectAndUpload();
+```
+
+
+Save images, text and data to device persistor
+
+```dart
+  String localPath = await DataPersistor().saveImage('/collectionName/Image.png',bytes);
+```
+
+Get images, text and data to device persistor
+
+```dart
+  String localPath = await DataPersistor().getImage('/collectionName/Image.png');
+```
+
+Remove images, text and data to device persistor
+
+```dart
+  String localPath = await DataPersistor().removeImage('/collectionName/Image.png');
+```
+
+
+## Additional information
+
+This package assumes complete Firebase configuration with Storage and permissions.
---
+++ lib/src/sound_library.dart
@@ -0,0 +1,77 @@
+import 'dart:typed_data';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:storage_manager/storage_manager.dart';
+
+enum Sounds { click, order_complete, success, welcome, wood_hit, add_to_cart, action, trash, drag }
+
+// Soundpool pool = Soundpool(streamType: StreamType.notification);
+class SoundPlayer {
+  bool enable = true;
+  Uint8List? buffer;
+  // -------  Here is the code to playback a remote file -----------------------
+  String _getAssetName(Sounds sound) {
+    String path;
+    switch (sound) {
+      case Sounds.click:
+        path = 'click.mp3';
+        break;
+      case Sounds.drag:
+        path = 'drag.mp3';
+        break;
+      case Sounds.trash:
+        path = 'trash.mp3';
+        break;
+      case Sounds.success:
+        path = 'success.mp3';
+        break;
+      case Sounds.welcome:
+        path = 'welcome.mp3';
+        break;
+      case Sounds.wood_hit:
+        path = 'wood_hit.mp3';
+        break;
+      case Sounds.order_complete:
+        path = 'order_complete.mp3';
+        break;
+      case Sounds.add_to_cart:
+        path = 'add_to_cart.mp3';
+        break;
+      case Sounds.action:
+        path = 'action.mp3';
+        break;
+      default:
+        path = 'click.mp3';
+    }
+    return path;
+  }
+
+  void play(Sounds sound) async {
+    if (instance.enable) {
+      AudioCache audioPlayer = AudioCache();
+      double volume = 1;
+      if (sound == Sounds.success) {
+        volume = .7;
+      }
+      await audioPlayer.play(_getAssetName(sound), volume: volume);
+    }
+  }
+
+  static Future<bool> checkAudioEnabled() async {
+    instance.enable = (await DataPersistor().getString('soundPermission') == 'true');
+    return instance.enable;
+  }
+
+  static setAudioEnabled(bool enable) async {
+    DataPersistor().saveString('soundPermission',enable?'true':'false');
+  }
+
+  static final SoundPlayer instance = SoundPlayer._internal();
+
+  SoundPlayer._internal();
+
+  factory SoundPlayer() {
+    // checkAudioPermission();
+    return instance;
+  }
+}
