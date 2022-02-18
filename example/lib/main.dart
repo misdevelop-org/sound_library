cdd611af7a8e216f40a6fd9b3b18e1e66cf5597b
Edgerik Leguizamon <edgerik.leguizamon@gmail.com>
2022-02-18 11:37:14 -0600

    Better example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,16 +15,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Sounds'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -33,30 +29,68 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black54,
+      backgroundColor: Colors.white70,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text("Sound Library"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            buildSoundButton('Click', Sounds.click),
+            buildSoundButton('Click ', Sounds.click,
+                icon: Icon(
+                  Icons.input,
+                  color: Colors.blue[800],
+                  size: 30,
+                )),
+            buildSoundButton('Welcome ', Sounds.welcome,
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.blue[800],
+                  size: 30,
+                )),
+            buildSoundButton('Wood hit ', Sounds.wood_hit),
+            buildSoundButton('Order Complete ', Sounds.order_complete,
+                icon: Icon(
+                  Icons.check,
+                  color: Colors.green[800],
+                  size: 30,
+                )),
+            buildSoundButton('Trash ', Sounds.trash,
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red[800],
+                  size: 30,
+                )),
+            buildSoundButton('Add to cart ', Sounds.add_to_cart,
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                  color: Colors.amber[800],
+                  size: 30,
+                )),
+            buildSoundButton('Success ', Sounds.success),
+            //Agroferia updates quantities and prices given by the producers every week
           ],
         ),
       ),
     );
   }
 
-  Widget buildSoundButton(String name, Sounds sound) => Padding(
+  Widget buildSoundButton(String name, Sounds sound, {Icon? icon}) => Padding(
         padding: const EdgeInsets.all(8.0),
         child: Card(
-          child: TextButton(
-            child: Text(name),
-            onPressed: () {
-              SoundPlayer.i.play(sound);
-            },
-          ),
+          child: icon != null
+              ? IconButton(
+                  onPressed: () {
+                    SoundPlayer.i.play(sound);
+                  },
+                  icon: icon)
+              : TextButton(
+                  child: Text(name),
+                  onPressed: () {
+                    SoundPlayer.i.play(sound);
+                  },
+                ),
         ),
       );
 }
